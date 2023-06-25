c011eb1b69957dc47ecb2362b73f55c54f299f5f
qfys521 <qfys520@vip.qq.com>
2023-06-25 11:49:38 +0800

    fix bug

--- src/main/java/cn/qfys521/xiaoming/plugin/Commands/AdvancedBanCommands.kt
+++ src/main/java/cn/qfys521/xiaoming/plugin/Commands/AdvancedBanCommands.kt
@@ -8,33 +8,33 @@ import cn.chuanwise.xiaoming.user.XiaoMingUser
 import cn.qfys521.xiaoming.plugin.AdvancedBanPlugin
 
 class AdvancedBanCommands : SimpleInteractors<AdvancedBanPlugin?>() {
-    @Filter(COMMANDHEAD + "ban {QQ}")
+    @Filter(COMMANDHEAD + "ban {qq}")
     @Required(PERMISSIONHEAD + "ban.add")
-    fun AddBanList(sender: XiaoMingUser<*>, @FilterParameter("QQ") qq: Long) {
+    fun AddBanList(sender: XiaoMingUser<*>, @FilterParameter("qq") qq: Long) {
         plugin!!.banList!!.banList.add(qq)
         sender.sendMessage("已将该用户添加至封禁名单。")
         plugin!!.banList!!.saveOrFail()
     }
 
-    @Filter(COMMANDHEAD + "whitelist add {QQ}")
+    @Filter(COMMANDHEAD + "whitelist add {qq}")
     @Required(PERMISSIONHEAD + "whitelist.add")
-    fun AddWhiteList(sender: XiaoMingUser<*>, @FilterParameter("QQ") qq: Long) {
+    fun AddWhiteList(sender: XiaoMingUser<*>, @FilterParameter("qq") qq: Long) {
         plugin!!.whiteList!!.whiteList.add(qq)
         sender.sendMessage("已将该用户添加至白名单。")
         plugin!!.whiteList!!.saveOrFail()
     }
 
-    @Filter(COMMANDHEAD + "unabn {QQ}")
+    @Filter(COMMANDHEAD + "unabn {qq}")
     @Required(PERMISSIONHEAD + "ban.remove")
-    fun RemoveBanList(sender: XiaoMingUser<*>, @FilterParameter("QQ") qq: Long) {
+    fun RemoveBanList(sender: XiaoMingUser<*>, @FilterParameter("qq") qq: Long) {
         plugin!!.banList!!.banList.remove(qq)
         sender.sendMessage("已将该用户移出封禁名单。")
         plugin!!.banList!!.saveOrFail()
     }
 
-    @Filter(COMMANDHEAD + "whitelist remove {QQ}")
+    @Filter(COMMANDHEAD + "whitelist remove {qq}")
     @Required(PERMISSIONHEAD + "whitelist.remove")
-    fun RemoveWhiteList(sender: XiaoMingUser<*>, @FilterParameter("QQ") qq: Long) {
+    fun RemoveWhiteList(sender: XiaoMingUser<*>, @FilterParameter("qq") qq: Long) {
         plugin!!.whiteList!!.whiteList.remove(qq)
         sender.sendMessage("已将该用户移除白名单。")
         plugin!!.whiteList!!.saveOrFail()
