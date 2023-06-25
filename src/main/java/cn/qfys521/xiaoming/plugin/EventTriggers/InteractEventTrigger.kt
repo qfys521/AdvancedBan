2856140d58e0a76ae82793e0283ba94f711f18fc
qfys521 <qfys520@vip.qq.com>
2023-06-25 12:03:00 +0800

    fix bug

--- src/main/java/cn/qfys521/xiaoming/plugin/EventTriggers/InteractEventTrigger.kt
+++ src/main/java/cn/qfys521/xiaoming/plugin/EventTriggers/InteractEventTrigger.kt
@@ -1,13 +1,14 @@
 package cn.qfys521.xiaoming.plugin.EventTriggers
 
 import cn.chuanwise.xiaoming.event.InteractEvent
+import cn.chuanwise.xiaoming.event.MessageEvent
 import cn.chuanwise.xiaoming.event.SimpleListeners
 import cn.qfys521.xiaoming.plugin.AdvancedBanPlugin
 
 class InteractEventTrigger : SimpleListeners<AdvancedBanPlugin>() {
-    fun onCommand(e: InteractEvent) {
-        val inWhiteList = plugin!!.whiteList?.whiteList?.contains(e.context.user.code)
-        val inBanList = plugin!!.banList?.banList?.contains(e.context.user.code)
+    fun onCommand(e: MessageEvent) {
+        val inWhiteList = plugin!!.whiteList?.whiteList?.contains(e.user.code)
+        val inBanList = plugin!!.banList?.banList?.contains(e.user.code)
         if (inWhiteList != true || inBanList == true) {
             e.cancel()
             return
